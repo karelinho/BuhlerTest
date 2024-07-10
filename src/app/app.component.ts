d7ae81a1fc1f5ecb9ac2a15271e727598ca6ff4c
Karel Sluka <karel.sluka@gmail.com>
2024-07-10 08:58:23 +0200

    Finish implementation of machine component.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,21 @@
-import { Component, ViewEncapsulation } from '@angular/core';
+import { ChangeDetectionStrategy, Component, ViewEncapsulation } from '@angular/core';
 import { DUMMY_MACHINES, Machine } from './dummy-machines';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
-  encapsulation: ViewEncapsulation.None
+  encapsulation: ViewEncapsulation.None,
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class AppComponent {
-  title = 'BuhlerTest';
-
+  /**
+   * Testing data of machines.
+   */
   machines: Machine[] = DUMMY_MACHINES;
+
+  /**
+   * Date used in toolbar.
+  */
+  today: number = Date.now();
 }
--- src/app/machine/machine.component.ts
+++ src/app/machine/machine.component.ts
@@ -1,20 +1,34 @@
-import { Component, Input, ViewEncapsulation } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Input, ViewEncapsulation } from '@angular/core';
 import { MachineState } from '../dummy-machines';
 
 @Component({
   selector: 'app-machine',
   templateUrl: './machine.component.html',
   styleUrl: './machine.component.css',
-  encapsulation: ViewEncapsulation.None
+  encapsulation: ViewEncapsulation.None,
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class MachineComponent {
+  /**
+   * Machine name.
+   */
   @Input({required: true}) name!: string;
 
+  /**
+   * Machine state.
+   */
   @Input({required: true}) state!: MachineState;
 
-  @Input() showIcon: boolean = true;
+  /**
+   * Whether to show icon above the name. Default is false.
+   */
+  @Input() showIcon: boolean = false;
 
-  public setClass() {
+  /**
+   * Returns an object containing css classes according to the machine state.
+   * @returns Object containing css classes.
+   */
+  public setStateClass() {
     return {
       'running': this.state === MachineState.running,
       'alarm' : this.state === MachineState.alarm,
@@ -22,14 +36,29 @@ export class MachineComponent {
     };
   }
 
+  /**
+   * Returns whether the state is running.
+   * @param state machine state
+   * @returns true when the state is running
+   */
   public isRunning(state: MachineState) {
     return state === MachineState.running;
   }
 
+  /**
+   * Returns whether the state is alarm.
+   * @param state machine state
+   * @returns true when the state is alarm
+   */
   public isAlarm(state: MachineState) {
     return state === MachineState.alarm;
   }
 
+  /**
+   * Returns whether the state is warning.
+   * @param state machine state
+   * @returns true when the state is warning
+   */
   public isWarning(state: MachineState) {
     return state === MachineState.warning;
   }
